ce4248df4651143ee3047b4e52a5f0aa9ae4d0dd
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-04-23 11:38:23 -0700

    added test minecrat api

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 Main Discord Bot File Use to link the project to gether the project
 """
 require 'discordrb'
-
+require 'mojang'
 
 prefix = "@"
 
@@ -24,6 +24,37 @@ bot.message(content: '@help') do |event|
     event << "`@minecraft` -> shows Status of the Minecraft Service's"
     event << "`@rockstar` -> Shows Status of the Rockstar Service's"
 end
+
+bot.message(content: '@steam') do |event|
+    
+end
+
+bot.message(content: '@xboxlive') do |event|
+    event <<"https://beta.support.xbox.com/xbox-live-status?xr=shellnav"
+end
+
+bot.message(content: '@uplay') do |event|
+    
+end
+
+bot.message(content: '@minecraft') do |event|
+  response = Curl.get('https://status.mojang.com/check'.freeze).body_str
+  json = Oj.load(response)
+  ret = {}  # Reformatting the returned data because it is super annoying to work with.
+
+  json.each do |hash|
+    hash.each do |site, status|
+      ret[site] = status 
+    end
+    end
+  event.respond(`session.minecraft.net` + ret['session.minecraft.net'])
+end
+
+bot.message(content: '@rockstar') do |event|
+    
+    
+end
+
 
 bot.join()
 
