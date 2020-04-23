47a757f7fa315381b65ed99a9236a7c9adfa8a42
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-04-23 08:18:55 -0700

    added help command list

--- main.rb
+++ main.rb
@@ -11,9 +11,21 @@ Logger.new(ENV['Starting Discord bot'])
 bot = Discordrb::Commands::CommandBot.new token: '', prefix: prefix
 bot.run true
 
+# Mentiond bot unction
 bot.mention do |event|
-    event.respond('hey u @me know u been atted')
+    event.respond('hey u @ me BOI IM TRYING TO  WORK')
 end
+
+bot.message(content: '@help') do |event|
+    event  << "`@help` - > Shows list of bot commands "
+    event << "`@steam` -> Shows Status of Steams Services"
+    event << "`@xboxlive` -> Shows Status of Steams Services"
+    event << "`@uplay` -> Shows Status of Uplays Services"
+    event << "`@minecraft` -> shows Status of the Minecraft Service's"
+    event << "`@rockstar` -> Shows Status of the Rockstar Service's"
+end
+
 bot.join()
+
 
     
